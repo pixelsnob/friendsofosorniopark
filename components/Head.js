ab715227e2b09a5d0fd511c4fd7bfda445499e64
Luis A. Echeverria <pixelsnob@gmail.com>
2020-01-13 14:50:14 -0800

    Site title update

--- components/Head.js
+++ components/Head.js
@@ -6,7 +6,7 @@ export default () => (
   <Fragment>
     
     <Head>
-      <title>Friends of Osornio Park | Osornio Park Belongs to the Community!</title>
+      <title>Friends of Osornio Park | La Habra, California</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link href="https://fonts.googleapis.com/css?family=Baloo+Bhaijaan|Montserrat:400,700&display=swap" rel="stylesheet"></link>
     </Head>
