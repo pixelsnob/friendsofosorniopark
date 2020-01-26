7aa9ec3d10a27b0620878f4ffa72f0c79d8b1029
Luis A. Echeverria <pixelsnob@gmail.com>
2020-01-26 15:33:02 -0800

    Added image, style tweaks

--- components/ImageContainer.js
+++ components/ImageContainer.js
@@ -18,14 +18,19 @@ export default (props) => {
           clear: both;
           width: ${props.width || '50vw'};
           margin: 20px 0 20px 20px;
+          
+          
         }
         figcaption {
           font-size: 0.9rem;
           line-height: 1.4;
           margin: 0;
+          padding: 0;
+          font-weight: bold;
         }
         img {
           width: 100%;
+          
         }
         @media screen and (max-width: 768px) {
           figure {
