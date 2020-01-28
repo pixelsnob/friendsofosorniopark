1d28ec326634e06b02dd27be926e3397102984fc
Luis A. Echeverria <pixelsnob@gmail.com>
2020-01-28 15:02:56 -0800

    Printable flyer

--- pages/flyer.js
+++ pages/flyer.js
@@ -14,7 +14,7 @@ export default () => (
       <article>
         <p>Osornio Park, a public park in La Habra, California, is not a friendly park. Since 2012, it has been taken over by City-affiliated sports teams, who use the City and other agencies to intimidate people out of coming to this park, especially if they have dogs.</p>
         <p>Residents have watched over the years while sports teams have taken advantage of their privilege by hanging advertising banners, leaving trash after games, parking wherever they please, holding events when the park is closed by the City for field maintenance, and confronting residents and other park visitors telling them what to do in a public space, and that they will get ticketed by the City for bringing dogs to this park -- even when the park is completely empty.</p>
-        <p>We are gathering signatures on a petition to present to the City of La Habra, which states the following:</p>
+        <p>We are gathering signatures on a petition to present to the City of La Habra that states the following:</p>
       </article>
      
       <article>
@@ -25,20 +25,27 @@ export default () => (
         </ol>
       </article>
       <article>
-        <p className="sign">To read and sign the full petition, please visit <strong>friendsofosorniopark.org</strong></p>
+        <p className="sign">To read and sign the full petition, visit <strong>friendsofosorniopark.org</strong></p>
       </article>
     </section>
     <footer>
       January 2020
     </footer>
     <style jsx>{`
+      @media print {
+        section {
+          border: 3px solid #999;
+          padding: 4.2vh 4.2vw 1vh 4.2vw;
+        }
+      }
+      
       h1 {
-        font-size: 4.3vh;
-        margin-bottom: 3vh;
+        font-size: 4vh;
+        margin-bottom: 2vh;
       }
       p, li {
         font-size: 2vh;
-        line-height: 1.6;
+        line-height: 1.4;
       }
       li {
         font-weight: bold;
@@ -49,7 +56,7 @@ export default () => (
       }
       footer {
         text-align: left;
-        font-size: 1.4vh;
+        font-size: 1.2vh;
       }
     `}</style>
   </Fragment> 
