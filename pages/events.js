f01e207b2b0ec0c808b8da320992183bfbc57ef2
Luis A. Echeverria <pixelsnob@gmail.com>
2020-03-01 19:33:55 -0800

    Cleanup, sitemap.xml date update

--- pages/events.js
+++ pages/events.js
@@ -1,46 +1,6 @@
-import { Fragment } from 'react';
+
 import Layout from '../components/Layout';
-import ImageContainer from '../components/ImageContainer';
-import FootnoteLink from '../components/FootnoteLink';
 import Head from '../components/Head';
-
-// const summary = 'Friends of Osornio Park is a group of residents with a common goal: to help make Osornio Park a more welcoming place for the entire community. We are not affiliated with the City of La Habra.';
-// const affiliatePolicyLink = 
-//   <a href="http://www.lahabracity.com/DocumentCenter/View/9984/City-of-La-Habra-Sport-Affiliate-Policy-PDF" target="_blank">
-//     City of La Habra Sports Affiliate Policy
-//   </a>
-// ;
-
-// const WrappedImageContainer = props => (
-//   <Fragment>
-//     <div className="image-container-wrapper">
-//       <ImageContainer {...props}/>
-//     </div>
-//     <style jsx>{`
-//       .image-container-wrapper {
-//         padding: 0;
-        
-//         float: right;
-//         clear: both;
-//         width: 50vw;
-//         margin: 30px 0 30px 30px;
-//       }
-      
-//       @media screen and (max-width: 768px) {
-//         .image-container-wrapper {
-//           float: none;
-//           width: 100%;
-//           margin: 20px 0;
-//         }
-//       }
-//       @media screen and (min-width: 1200px) {
-//         .image-container-wrapper {
-//           width: 40vw;
-//         }
-//       }
-//     `}</style>
-//   </Fragment>
-// );
 
 export default () => (
   <Layout showBackToTopLink={true}>
