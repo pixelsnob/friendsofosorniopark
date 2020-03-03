36c78f3f7fc0f5d735b2c46150852b710189814e
Luis A. Echeverria <pixelsnob@gmail.com>
2020-03-02 19:02:35 -0800

    next.js <Link> for client-side navigation

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,5 @@
 import { Fragment } from 'react';
-//import Head from './Head';
+import Link from 'next/link';
 import Footer from './Footer';
 import Nav from './Nav';
 import BackToTopLink from './BackToTopLink';
@@ -8,7 +8,7 @@ import BackToTopLink from './BackToTopLink';
 export default props => (
   <Fragment>
     
-    <h1 className="noprint"><a href="/" title="Friends of Osornio Park">Friends of Osornio Park</a></h1>
+    <h1 className="noprint"><Link href="/"><a title="Friends of Osornio Park">Friends of Osornio Park</a></Link></h1>
     <div className="content">
       <div className="nav-container">
         <Nav/>
