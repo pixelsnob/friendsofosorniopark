5d62ecc104a4d01e4e40d5dc8353a54ed075e150
Luis A. Echeverria <pixelsnob@gmail.com>
2020-03-04 13:38:12 -0800

    FootnoteLink tweak

--- components/FootnoteLink.js
+++ components/FootnoteLink.js
@@ -1,4 +1,4 @@
 
 export default props => {
-  return <sup><a className="footnote" href={"#" + props.name} name={props.name}></a></sup>
+  return <sup><a className="footnote" href={"#" + props.href} name={props.name}></a></sup>
 }
